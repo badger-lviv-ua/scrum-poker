91ff6679abd8b1757bd58c9791795688f55d2541
BadgerUA <badger.lviv.ua@gmail.com>
2023-05-11 21:28:07 +0300

    Firebase config env

---
+++ src/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+
+export const firebaseApp = initializeApp({
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
+})
